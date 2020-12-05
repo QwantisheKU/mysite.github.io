7c9b21aa627a29173dbdd608f1ca6cfde2c6e29e
Irene Veselova <ik1vikuz@gmail.com>
2020-12-05 23:31:52 +0300

    Some fixes

---
+++ Курсач/index.js
@@ -0,0 +1,7 @@
+const mainFrame = document.querySelector('#main');
+const sideFrame = document.querySelector('#menuframe');
+
+window.addEventListener("load", function () {
+    mainFrame.style.height = '100%';
+    sideFrame.style.height = '68%';
+});
